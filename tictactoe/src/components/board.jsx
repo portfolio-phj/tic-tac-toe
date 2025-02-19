2142b0437f2e9fd265ff7ef75d9e20e846875e32
hello-phj <hejae0419@gmail.com>
2025-02-19 23:00:04 +0900

    tic-tac-toe:css

--- tictactoe/src/components/board.jsx
+++ tictactoe/src/components/board.jsx
@@ -4,20 +4,23 @@ import "../css/board.css";
 function Board() {
   return (
     <div id="board-background">
-      <div className="board-line">
-        <Square value={1}></Square>
-        <Square value={2}></Square>
-        <Square value={3}></Square>
-      </div>
-      <div className="board-line">
-        <Square value={4}></Square>
-        <Square value={5}></Square>
-        <Square value={6}></Square>
-      </div>
-      <div className="board-line">
-        <Square value={7}></Square>
-        <Square value={8}></Square>
-        <Square value={9}></Square>
+      <p id="board-title">TIC-TAC-TOE</p>
+      <div id="board-back">
+        <div className="board-line">
+          <Square value={1}></Square>
+          <Square value={2}></Square>
+          <Square value={3}></Square>
+        </div>
+        <div className="board-line">
+          <Square value={4}></Square>
+          <Square value={5}></Square>
+          <Square value={6}></Square>
+        </div>
+        <div className="board-line">
+          <Square value={7}></Square>
+          <Square value={8}></Square>
+          <Square value={9}></Square>
+        </div>
       </div>
     </div>
   );
